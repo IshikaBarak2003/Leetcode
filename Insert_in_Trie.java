ad18f6c86f4f90db1f16e99b62df501272b6649d
IshikaBarak2003 <ishika.barak2020@vitbhopal.ac.in>
2023-05-04 01:16:27 +0530

    new commit

---
+++ Insert_in_Trie.java
@@ -0,0 +1,55 @@
+class TrieNode {
+    boolean eow;
+    TrieNode[] children;
+    
+    public TrieNode() {
+        eow = false;
+        children = new TrieNode[26]; // 26 English lowercase letters
+    }
+}
+
+class Trie {
+    TrieNode root;
+    
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index];
+        }
+        node.eow = true;
+    }
+    
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return node.eow;
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return true;
+    }
+}
+
+
